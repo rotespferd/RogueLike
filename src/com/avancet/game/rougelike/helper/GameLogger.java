28674a5f97c42a8958b031e9e102477603f9b5d9
Marian Sievers <marian.sievers@gmail.com>
2011-08-25 21:51:12 +0200

    Add log4j.jar and GameLogger.
    Signed-off-by: Marian Sievers <marian.sievers@gmail.com>

---
+++ src/com/avancet/game/rougelike/helper/GameLogger.java
@@ -0,0 +1,18 @@
+package com.avancet.game.rougelike.helper;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class GameLogger {
+	
+	private static Logger logger = null;
+
+	public Logger getLogger(){
+		if(logger == null){
+			logger  = Logger.getLogger(GameLogger.class);
+			PropertyConfigurator.configureAndWatch("config/log4j.properties", 2000);
+			logger.info("GameLogger is initialized.");
+		}
+		return logger;
+	}
+}
